e335d908a695f7a2b6cc650239ce5e94ab63dce2
Your Name <you@example.com>
2018-01-04 13:22:10 +0700

    ada index

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>	
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Konsisten</title>
+
+		<!-- Bootstrap CSS file -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link href="css/bootstrap-theme.min.css" rel="stylesheet" />
+		<link href="css/blog.css" rel="stylesheet" />
+	</head>
+
+	<body>
+
+		<!-- Header -->
+		        <nav class="navbar navbar-inverse" role="navigation">
+		    	  <div class="container">
+				    <!-- Brand and toggle get grouped for better mobile display -->
+				    <div class="navbar-header">
+				      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				      <div class="navbar-brand navbar-brand-centered">Brand</div>
+				    </div>
+
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				    <div class="collapse navbar-collapse" id="navbar-brand-centered">
+				      <ul class="nav navbar-nav">
+				        <li><a href="#">Link</a></li>
+				        <li><a href="#">Link</a></li>
+				        <li><a href="#">Link</a></li>
+				      </ul>
+				      <ul class="nav navbar-nav navbar-right">
+				        <li><a href="#">Link</a></li>
+				        <li><a href="#">Link</a></li>
+				        <li><a href="#">Link</a></li>		        
+				      </ul>
+				    </div><!-- /.navbar-collapse -->
+				  </div><!-- /.container-fluid -->
+				</nav>
+		<!-- Body -->
+		
+		<!-- Footer -->
+
+		<!-- Bootstrap Script file -->
+		<script src="js/jquery-2.0.3.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+	</html>
